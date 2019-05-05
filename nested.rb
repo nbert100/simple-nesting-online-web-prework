ba181186aec1e4fcd2e6c3e71b6ddf64448c8d68
Nia Bert <nbert14@gmail.com>
2019-05-05 21:21:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,7 +125,7 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages] = 
+     programmer_hash[:dennis_ritchie][:languages] = ["C"]
      return programmer_hash
 
 end
